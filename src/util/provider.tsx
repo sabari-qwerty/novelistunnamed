8386216a3da52e074b54974b9bea9ffbd0ba60c4
sabari-qwerty <120260499+sabari-qwerty@users.noreply.github.com>
2023-08-14 09:00:43 +0530

    remove devtools

--- src/util/provider.tsx
+++ src/util/provider.tsx
@@ -1,17 +1,11 @@
 "use client";
 import { FC, ReactNode, useState } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 interface Provider {
   children: ReactNode;
 }
 
 export const Provider: FC<Provider> = ({ children }) => {
   const [client] = useState(new QueryClient());
-  return (
-    <QueryClientProvider client={client}>
-      {children}
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
-  );
+  return <QueryClientProvider client={client}>{children}</QueryClientProvider>;
 };
