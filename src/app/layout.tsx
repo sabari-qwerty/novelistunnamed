3477649db1e32d7afd1f70f384e47d045d76f659
sabari-qwerty <120260499+sabari-qwerty@users.noreply.github.com>
2023-08-18 08:32:57 +0530

    update data fount

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import "./globals.css";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+// import { Inter } from "next/font/google";
 import { Provider } from "@/util/provider";
 
-const inter = Inter({ subsets: ["latin"] });
+// const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -17,7 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-[#1d232a]`}>
+      <body className={` bg-[#1d232a]`}>
         <Provider>{children}</Provider>
       </body>
     </html>
